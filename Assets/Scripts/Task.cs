04a1c44a136586ea8e5fcb1ea9fe829e8d46d94c
Lesther <kpable7@gmail.com>
2017-12-02 10:23:33 -0500

    A Start

---
+++ Assets/Scripts/Task.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TaskStatus { Pending, Skipped, Partial, Success }
+
+public class Task {
+
+    public int[] solution;
+    public int[] entry;
+    public TaskStatus status;
+    public WorkType type;
+
+    public Task(WorkType type, int[] entryOptions)
+    {
+        this.type = type;
+        SetSolution(entryOptions);
+        Debug.Log("Task created: Type - " + type.ToString() + " Solution - " + solution.ArrayOutput());
+    }
+
+    void SetSolution(int[] entryOptions)
+    {
+        switch (type)
+        {
+            case WorkType.Programs:
+                // Solution will be 4 random entries
+                solution = new int[4];
+                break;
+            case WorkType.Circuits:
+                // Solution will be 4 random entries
+                solution = new int[4];               
+                break;
+            case WorkType.Manuals:
+                // Solution will be 2 random entries
+                solution = new int[2];
+                break;
+            default:
+                break;
+        }
+
+        for (int i = 0; i < solution.Length; i++)
+        {
+            solution[i] = entryOptions[Random.Range(0, entryOptions.Length)];
+        }
+    }
+}
---
+++ Assets/Scripts/Workstation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum WorkType { Programs, Circuits, Manuals }
+
+public class Workstation : MonoBehaviour {
+
+    public WorkType type;
+    public Sprite[] entries;
+    public List<Task> inputTasks = new List<Task>();
+    public List<Task> outputTasks = new List<Task>();
+
+    Task currentTask;
+    int[] entryOptions;
+
+    // Use this for initialization
+    void Start () {
+        entryOptions = new int[entries.Length];
+
+        for (int i = 0; i < entryOptions.Length; i++)
+        {
+            entryOptions[i] = i;
+        }
+
+        StartCoroutine("AddTask");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator AddTask()
+    {
+        inputTasks.Add(new Task(type, entryOptions));
+        yield return new WaitForSeconds(Random.Range(3, 5));
+        StartCoroutine("AddTask");
+    }
+}
