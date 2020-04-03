d0765ebddb657d3707fe3472b4f9e352e07fd7f1
lestherreynoso <lhreynos@gmail.com>
2020-04-02 21:37:42 -0400

    Style cop fixes

--- Assets/Scripts/GameFlowController.cs
+++ Assets/Scripts/GameFlowController.cs
@@ -3,66 +3,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controls the Game Flow.
+/// </summary>
 public class GameFlowController : MonoBehaviour
 {
     [SerializeField]
     private InGameClock inGameClock;
-
+    
+    /// <summary>
+    /// Called first. 
+    /// </summary>
     private void Awake()
     {
-        if(!inGameClock)
+        if (!inGameClock)
         {
             Debug.LogError(name + ": InGameClock is not linked");
             return;
         }
 
+        inGameClock.RegisterTimedCallback(new TimeSpan(0, 7, 0, 0), PreWork);
         inGameClock.RegisterTimedCallback(new TimeSpan(0, 8, 0, 0), Morning);
         inGameClock.RegisterTimedCallback(new TimeSpan(0, 12, 0, 0), Lunch);
         inGameClock.RegisterTimedCallback(new TimeSpan(0, 13, 0, 0), Afternoon);
         inGameClock.RegisterTimedCallback(new TimeSpan(0, 17, 0, 0), PostWork);
-
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    /// <summary>
+    /// Time of Day
+    /// </summary>
     private void PreWork()
     {
         Debug.Log("Its pre work time!");
-
     }
 
+    /// <summary>
+    /// Time of Day
+    /// </summary>
     private void Morning()
     {
         Debug.Log("Its morning time!");
-
     }
 
+    /// <summary>
+    /// Time of Day
+    /// </summary>
     private void Lunch()
     {
         Debug.Log("Its lunch time!");
     }
 
+    /// <summary>
+    /// Time of Day
+    /// </summary>
     private void Afternoon()
     {
         Debug.Log("Its noon time!");
-
     }
 
+    /// <summary>
+    /// Time of Day
+    /// </summary>
     private void PostWork()
     {
         Debug.Log("Its after work time!");
-
     }
-
-
 }
